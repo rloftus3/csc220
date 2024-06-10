0913a3b6fabdcf9034053ebf857e752645251753
rloftus3 <162064076+rloftus3@users.noreply.github.com>
2024-06-10 03:34:16 +0000

    change username

--- 00-lab/name.py
+++ 00-lab/name.py
@@ -3,7 +3,7 @@ import sys
 sys.path.append('/home/staff/kurban/python')
 import csc220
 
-print("My name is Rachel!")
+print("My username is rloftus3")
 
 # I honor Parkland's core values by affirming that I have 
 # followed all academic integrity guidelines for this work.
