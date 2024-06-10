1f35e221aa12f59685db8d719d86bd51d4eab9b3
rloftus3 <162064076+rloftus3@users.noreply.github.com>
2024-06-10 03:31:57 +0000

    fix kenneth's garbage

--- 01-lab/shapes.py
+++ 01-lab/shapes.py
@@ -1,20 +1,7 @@
 #!/usr/local/bin/python3
 import sys
 sys.path.append('/home/staff/kurban/python')
-
 import csc220
-
-csc220.showForm("This is the comment on the form area.")  
-
-textarea = csc220.getInput('textarea')
-textbox = csc220.getInput('textbox')
-
-print("<h2>This is at the bottom and can be used for any html output </h2><br>")
-
-
-
-print("textbox contains <b>{}</b> <br>".format( textbox ))
-print("textarea contains <b>{}</b> <br>".format( textarea ))
 
 def circleLeft():
     return """
