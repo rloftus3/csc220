8de91e03273cd3c4bcc37dbc74fe7c129bab7180
rloftus3 <lunarscaped@gmail.com>
2024-06-09 22:15:57 -0500

    fix name.py

--- 00-lab/name.py
+++ 00-lab/name.py
@@ -9,15 +9,16 @@ csc220.showForm("This is the comment on the form area.")
 textarea = csc220.getInput('textarea')
 textbox = csc220.getInput('textbox')
 
-print("<h2>This is at the bottom and can be used for any html output </h2><br>")
+print ("<h2>This is at the bottom and can be used for any html output </h2><br>")
 
-print("textbox contains <b>{}</b> <br>".format( textbox ))
-print("textarea contains <b>{}</b> <br>".format( textarea ))
+print ("textbox contains <b>{}</b> <br>".format( textbox ))
+print ("textarea contains <b>{}</b> <br>".format( textarea ))
 
-print("My username is rloftus3.")
+print("My name is Rachel!")
 
 # I honor Parkland's core values by affirming that I have 
 # followed all academic integrity guidelines for this work.
 
-# Rachel Loftus 
+# your name 
 # there is nothing below here!
+
