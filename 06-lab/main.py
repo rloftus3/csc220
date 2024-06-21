31f51becd304777f517450b4b250ba65812d9978
rloftus3 <162064076+rloftus3@users.noreply.github.com>
2024-06-21 17:50:38 +0000

    done!

--- 06-lab/main.py
+++ 06-lab/main.py
@@ -5,3 +5,19 @@ from random import randint
 def randList(size, min, max):
     return list(randint(min, max) for i in range(size))
 
+# Testing quick sort
+# data = [2, 3, 4, 5, 7]
+# quicksort.inplace_quick_sort(data, 0, len(data) - 1)
+# print(quicksort.listVisits)
+
+for i in range(300):
+    size = randint(1000, 5000)
+    thelist = randList(size, 0, 99)
+    listVisits = quicksort.inplace_quick_sort(thelist)
+    print(f"{size}, {listVisits}")
+
+# I honor Parkland's core values by affirming that I have 
+# followed all academic integrity guidelines for this work.
+
+# Rachel Loftus
+# there is nothing below here!
--- 06-lab/quicksort.py
+++ 06-lab/quicksort.py
@@ -19,27 +19,46 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+listVisits = 0
+
 def inplace_quick_sort(S, a, b):
   """Sort the list from S[a] to S[b] inclusive using the quick-sort algorithm."""
+  global listVisits
   if a >= b: return                                      # range is trivially sorted
+
+  listVisits += 1
   pivot = S[b]                                           # last element of range is pivot
   left = a                                               # will scan rightward
   right = b-1                                            # will scan leftward
   while left <= right:
     # scan until reaching value equal or larger than pivot (or right marker)
-    while left <= right and S[left] < pivot:
-      left += 1
+    while left <= right:
+      listVisits += 1
+      if S[left] < pivot:
+        left += 1
+      else: 
+        break
     # scan until reaching value equal or smaller than pivot (or left marker)
-    while left <= right and pivot < S[right]:
-      right -= 1
+    while left <= right:
+      listVisits += 1
+      if pivot < S[right]:
+        right -= 1
+      else:
+        break
     if left <= right:                                    # scans did not strictly cross
+      listVisits += 4
       S[left], S[right] = S[right], S[left]              # swap values
       left, right = left + 1, right - 1                  # shrink range
 
   # put pivot into its final place (currently marked by left index)
+  listVisits += 4
   S[left], S[b] = S[b], S[left]
   # make recursive calls
   inplace_quick_sort(S, a, left - 1)
   inplace_quick_sort(S, left + 1, b)
 
+# I honor Parkland's core values by affirming that I have 
+# followed all academic integrity guidelines for this work.
 
+# Rachel Loftus
+# there is nothing below here!
