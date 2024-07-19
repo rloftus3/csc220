442b85fd0d18dbda4ae94666a93b15de0ec30e21
rloftus3 <162064076+rloftus3@users.noreply.github.com>
2024-07-19 16:45:21 +0000

    import class code

---
+++ 20-lab/main.py
@@ -0,0 +1,60 @@
+#!/usr/local/bin/python3
+import csc220
+import graph
+import dfs
+from tabulate import tabulate
+csc220.showForm("<br/>")
+
+data = csc220.getInput('textarea')
+lines = data.split('\n')
+g = graph.Graph(True)
+vNames = []
+eNames = []
+foundEdges = False
+
+for line in lines:
+    if line == "#end":
+        foundEdges = True
+        continue
+    if foundEdges:
+        eNames.append(line)
+    else:
+        vNames.append(line)
+
+verts = {}
+for vName in vNames:
+    if vName in verts:
+        error = f"Duplicate vertex name: {vName}."
+        print(f"<pre> {error} </pre>")
+        raise UserWarning(error)
+    verts[vName] = g.insert_vertex(vName)
+for eName in eNames:
+    parts = eName.split(', ')
+    try:
+        v1 = verts[parts[0]]
+        v2 = verts[parts[1]]
+    except KeyError as e:
+        print(f"Invalid endpoint: {e}.") 
+        continue
+    else:
+        g.insert_edge(v1, v2)
+
+# build table structure for output
+table = [["Vertex", "Outgoing Edges"]]
+
+for v in g.vertices():
+    edges = []
+    for e in g.incident_edges(v, outgoing=True):
+        edges.append(e.opposite(v).element())
+    table.append([v.element(), "<br/>".join(edges)])
+html_table = tabulate(table, headers = "firstrow", tablefmt = "unsafehtml")
+
+# print html table output
+print(f"<div> {html_table} </div>")
+
+
+
+# I honor Parkland's core values by affirming that I have 
+# followed all academic integrity guidelines for this work.
+
+# Rachel Loftus
